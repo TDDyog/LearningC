441e95506c3b8928a4a9c857e45d55c021d17acf
cMwaa <126564592+TDDyog@users.noreply.github.com>
2025-12-06 09:49:27 +0000

    HelloWorldCPP

---
+++ Hello_World/C/HelloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello World"); //This prints the outputs, each appearing on a new ligne.
+	return 0;
+}
---
+++ Hello_World/CPP/HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World !" << endl << "comment allez-vous ?" << endl; //This prints the outputs, each appearing on a new ligne.
+    return 0;
+}
